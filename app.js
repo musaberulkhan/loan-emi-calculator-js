950580140890ccb9f0f90090b51ccf55a3f4a127
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-09-03 14:05:09 +0600

    Chart JS CDN and Demo Added

--- app.js
+++ app.js
@@ -88,3 +88,23 @@ calculateButton.addEventListener('click', function () {
 });
 
 
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{           
+            data: [19, 19],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'             
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'                
+            ],
+            borderWidth: 1
+        }]
+    }    
+});
+
